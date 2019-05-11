e0867d400d35cf3eb59def78035da4fd7bdbe85b
xx3u <yuliyahvan@yahoo.com>
2019-05-11 18:02:29 +0600

    LoginActivity added

---
+++ app/src/main/java/kz/androidgirls/team/ui/login/LoginActivity.kt
@@ -0,0 +1,13 @@
+package kz.androidgirls.team.ui.login
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kz.androidgirls.team.R
+
+class LoginActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
