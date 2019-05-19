0c2635fa12b30c187316ce119d798b92eac2dd3f
assel812 <assel812@mail.ru>
2019-05-20 00:05:27 +0600

    fix bug loginactivity file

--- app/src/main/java/kz/androidgirls/team/ui/login/LoginActivity.kt
+++ app/src/main/java/kz/androidgirls/team/ui/login/LoginActivity.kt
@@ -2,6 +2,7 @@ package kz.androidgirls.team.ui.login
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kz.androidgirls.team.R
 
 class LoginActivity : AppCompatActivity() {
 
